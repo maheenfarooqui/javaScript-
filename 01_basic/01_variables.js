c2f84fb83e0dfd7232ae8eb991b9697a26b230dc
maheenfarooqui <maheenzuhra@gmail.com>
2025-01-01 16:34:05 +0000

    vaariables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "maew@.com"
+var accountPassword = "09876"
+accountCity = "karachi"
+
+//accountId = 09 not allowed //
+
+
+
+accountEmail = "mnkk@.com"
+accountPassword = "09656"
+accountCity = "kari"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
